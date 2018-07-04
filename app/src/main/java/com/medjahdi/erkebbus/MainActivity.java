f4ab0203ee5adedbcf69de5487befd6c3914268c
Karam Medjahdi <medjahdi.karam@gmail.com>
2018-07-04 01:50:36 +0100

    Added the firebase dependencies to the bus project

--- app/src/main/java/com/medjahdi/erkebbus/MainActivity.java
+++ app/src/main/java/com/medjahdi/erkebbus/MainActivity.java
@@ -1,13 +1,23 @@
 package com.medjahdi.erkebbus;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
